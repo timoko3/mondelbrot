4a388a73396d775b49d2c571b278f397fbc4e6d0
timoko3 <freepower27@gmail.com>
2026-04-05 18:32:14 +0300

    working x6 speed up version

--- main.cpp
+++ main.cpp
@@ -41,18 +41,18 @@ int main(void){
     float curYPosScaled   = 0;
 
     debugPrintMask16(0x0f0f, "test");
-    while(time < 1){
+    while(time < 10000){
         txLock();
-        for(int counterX = 0; counterX < (int) WIDTH_WINDOW; counterX++){
-            curXPosScaled = (counterX - 1000.0f) / SCALE_NUM;
-            for(int counterY = 0; counterY < (int) HEIGHT_WINDOW; counterY += UNWRAP_NUMBER){
-                curYPosScaled = (counterY - 300.0f)  / SCALE_NUM;
+        for(int counterY = 0; counterY < (int) HEIGHT_WINDOW; counterY++){
+            curYPosScaled = (counterY - 300.0f)  / SCALE_NUM;
+            for(int counterX = 0; counterX < (int) WIDTH_WINDOW; counterX += UNWRAP_NUMBER){
+                curXPosScaled = (counterX - 1000.0f) / SCALE_NUM;
 
-                __m512 curShiftRe = _mm512_set1_ps(curXPosScaled);                         
-                __m512 curShiftIm = _mm512_set_ps(curYPosScaled + 15 * dy, curYPosScaled + 14 * dy, curYPosScaled + 13 * dy, curYPosScaled + 12 * dy,
-                                                curYPosScaled + 11 * dy, curYPosScaled + 10 * dy, curYPosScaled + 9 * dy,  curYPosScaled + 8 * dy,
-                                                curYPosScaled + 7 * dy,  curYPosScaled + 6 * dy,  curYPosScaled + 5 * dy,  curYPosScaled + 4 * dy,
-                                                curYPosScaled + 3 * dy,  curYPosScaled + 2 * dy,  curYPosScaled + dy,      curYPosScaled);
+                __m512 curShiftRe = _mm512_set_ps(curXPosScaled + 15 * dx, curXPosScaled + 14 * dx, curXPosScaled + 13 * dx, curXPosScaled + 12 * dx,
+                    curXPosScaled + 11 * dx, curXPosScaled + 10 * dx, curXPosScaled + 9 * dx,  curXPosScaled + 8 * dx,
+                    curXPosScaled + 7 * dx,  curXPosScaled + 6 * dx,  curXPosScaled + 5 * dx,  curXPosScaled + 4 * dx,
+                    curXPosScaled + 3 * dx,  curXPosScaled + 2 * dx,  curXPosScaled + dx,      curXPosScaled);
+                __m512 curShiftIm = _mm512_set1_ps(curYPosScaled);                         
 
                 __m512 curComplexRe = curShiftRe;
                 __m512 curComplexIm = curShiftIm;
@@ -60,40 +60,59 @@ int main(void){
                 __m512i exitIndexes = _mm512_set1_epi32(0);
 
                 int curIter = 0;
-                if(HEIGHT_WINDOW - counterY > 16){
+                if(WIDTH_WINDOW - counterX > UNWRAP_NUMBER){
                     while(curIter < AMOUNT_ITERATIONS){
-
+                        // if(curIter == 0) debugPrintV512(curComplexRe,     "curComplexRe");
+                        // if(curIter == 0) debugPrintV512(curComplexIm,     "curComplexIm");
+                        
                         __m512 curComplexReSquare = _mm512_mul_ps(curComplexRe, curComplexRe);
                         __m512 curComplexImSquare = _mm512_mul_ps(curComplexIm, curComplexIm);
                         __m512 curComplexImRe     = _mm512_mul_ps(curComplexRe, curComplexIm);
 
                         __m512 curComplexSquare   = _mm512_add_ps(curComplexReSquare, curComplexImSquare);
 
+                        // if(curIter == 0) debugPrintV512(curComplexReSquare, "curComplexReSquare");
+                        // if(curIter == 0) debugPrintV512(curComplexImSquare, "curComplexImSquare");
+                        // if(curIter == 0) debugPrintV512(curComplexImRe,     "curComplexImRe");
+                        // if(curIter == 0) debugPrintV512(curComplexSquare,   "curComplexSquare");
 
                         __m512 unwNumber        =  _mm512_set1_ps(MAX_COMPLEX_NUM_SIZE);
                         __mmask16 mask          = _mm512_mask_cmp_ps_mask(0xFFFF, curComplexSquare, unwNumber, _CMP_LT_OS);
                         exitIndexes             = _mm512_mask_add_epi32(exitIndexes, mask, exitIndexes, _mm512_set1_epi32(1));
                         
-                        debugPrintMask16(mask, "Mask");
+                        if (_kortestz_mask16_u8(mask, mask)) break;
 
-                        curComplexRe            = _mm512_mask_sub_ps(curComplexRe, mask, curComplexReSquare, curComplexImSquare);
-                        curComplexRe            = _mm512_mask_add_ps(curComplexRe, mask, curComplexRe, curShiftRe);
+                        // if(curIter == 0) debugPrintMask16(mask, "Mask");
 
-                        curComplexIm            = _mm512_mask_add_ps(curComplexIm, mask, curComplexImRe, curComplexImRe);
-                        curComplexIm            = _mm512_mask_add_ps(curComplexIm, mask, curComplexIm, curShiftIm);
+                        __m512 newRe = _mm512_sub_ps(curComplexReSquare, curComplexImSquare);
+                        newRe = _mm512_add_ps(newRe, curShiftRe);
 
+                        __m512 newIm = _mm512_add_ps(curComplexImRe, curComplexImRe);
+                        newIm = _mm512_add_ps(newIm, curShiftIm);
+
+                        curComplexRe = _mm512_mask_mov_ps(curComplexRe, mask, newRe);
+                        curComplexIm = _mm512_mask_mov_ps(curComplexIm, mask, newIm);
+
+                        // if(curIter == 0) debugPrintV512(curComplexRe,     "curComplexRe");
+                        // if(curIter == 0) debugPrintV512(curComplexIm,     "curComplexIm");
+                        
+                        // if(curIter == 0) lprintf("---------------------------------------------\n");
                         curIter++;
                     }
                     
-                    __m512i v_red   = _mm512_and_epi32(exitIndexes, _mm512_set1_epi32(113)); 
-                    __m512i v_green = _mm512_and_epi32(exitIndexes, _mm512_set1_epi32(127)); 
-                    __m512i v_blue  = _mm512_and_epi32(exitIndexes, _mm512_set1_epi32(10));  
+                    __m512i v_red   = _mm512_and_epi32(_mm512_mullo_epi32(exitIndexes, _mm512_set1_epi32(5)),  _mm512_set1_epi32(255));
+                    __m512i v_green = _mm512_and_epi32(_mm512_mullo_epi32(exitIndexes, _mm512_set1_epi32(9)),  _mm512_set1_epi32(255));
+                    __m512i v_blue  = _mm512_and_epi32(_mm512_mullo_epi32(exitIndexes, _mm512_set1_epi32(13)), _mm512_set1_epi32(255));
+
+                    // __m512i v_red      =  exitIndexes;
+                    // __m512i v_green    =  exitIndexes;
+                    // __m512i v_blue     =  exitIndexes;
 
                     __m512i v_pixel = _mm512_or_si512(_mm512_slli_epi32(v_red, 16), _mm512_or_si512(_mm512_slli_epi32(v_green, 8), v_blue));
 
                     void* dest_addr = &videoMemBuffer[counterX + (-(counterY) + HEIGHT_WINDOW - 1) * WIDTH_WINDOW];
 
-                    _mm512_storeu_si512((__m512i*)dest_addr, v_pixel);
+                    _mm512_store_si512((__m512i*)dest_addr, v_pixel);
                 }
             }
         }
