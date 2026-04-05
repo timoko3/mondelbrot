cc6603e6b959aef2f47d4fed148ba85c01ffabc3
timoko3 <freepower27@gmail.com>
2026-04-05 14:20:37 +0300

    first optimization

--- main.cpp
+++ main.cpp
@@ -9,16 +9,18 @@
 #include <assert.h>
 
 
-const size_t WIDTH_WINDOW  = 800;
-const size_t HEIGHT_WINDOW = 600;
+const size_t WIDTH_WINDOW         = 800;
+const size_t HEIGHT_WINDOW        = 600;
 
-const float  dx            = 1 / WIDTH_WINDOW;
-const float  dY            = 1 / HEIGHT_WINDOW;
+const float  SCALE_NUM            = 450.0;
 
-const size_t AMOUNT_ITERATIONS = 256;
+const float  dx                   = 1 / SCALE_NUM;
+const float  dy                   = 1 / SCALE_NUM;
 
-const size_t UNWRAP_NUMBER     = 4;
-const size_t SIZE_OF_ARRAY     = UNWRAP_NUMBER * 2;   // for 8 complex numbers
+const float  MAX_COMPLEX_NUM_SIZE = 4.0;
+const size_t AMOUNT_ITERATIONS    = 256;
+
+const size_t UNWRAP_NUMBER        = 4;
 
 inline void countMondelbrot(float* curComplex, float* curShift, int* exitIndexes, int curIter, char* flag);
 inline void drawMondelbrot(RGBQUAD** videoMemBuffer, int curIter, int counterX, int counterY);
@@ -30,55 +32,55 @@ int main(void){
 
     float time   = 0;
     float fps    = 0;
-    float curX   = 0;
-    float curY   = 0;
+    float curXPosScaled   = 0;
+    float curYPosScaled   = 0;
     while(true){
         txLock();
-        for(int counterX = 0; counterX < (int) WIDTH_WINDOW; counterX += UNWRAP_NUMBER, curX += UNWRAP_NUMBER * dx){
-            float x0[UNWRAP_NUMBER] = {curX, curX + 1, curX + 2, curX + 3};
-            float y0[UNWRAP_NUMBER] = {curY, curY, curY, curY};
+        for(int counterX = 0; counterX < (int) WIDTH_WINDOW; counterX++){
+            curXPosScaled = (counterX - 1000.0f) / SCALE_NUM;
+            for(int counterY = 0; counterY < (int) HEIGHT_WINDOW; counterY += UNWRAP_NUMBER){
+                curYPosScaled = (counterY - 300.0f)  / SCALE_NUM;
 
-            float curShiftX[UNWRAP_NUMBER] = {}; for(int i = 0; i < UNWRAP_NUMBER; i++) curShiftX[i] = x0[i];
-            float curShiftY[UNWRAP_NUMBER] = {}; for(int i = 0; i < UNWRAP_NUMBER; i++) curShiftX[i] = y0[i];
+                float curShiftRe[UNWRAP_NUMBER]         = {curXPosScaled, curXPosScaled, curXPosScaled, curXPosScaled};
+                float curShiftIm[UNWRAP_NUMBER]         = {curYPosScaled, curYPosScaled + dy, curYPosScaled + 2 * dy, curYPosScaled + 3 * dy};
 
-            for(int counterY = 0; counterY < (int) HEIGHT_WINDOW; counterY += 8){
-                float curComplex[SIZE_OF_ARRAY] = {0.0}; 
-                int   exitIndexes[UNWRAP_NUMBER] = {0};
-                // for(int i = 0; i < UNWRAP_NUMBER; i++) exitIndexes[i] = AMOUNT_ITERATIONS;
+                float curComplexRe[UNWRAP_NUMBER]       = {0.0}; for(int i = 0; i < UNWRAP_NUMBER; i++) curComplexRe[i] = curShiftRe[i]; 
+                float curComplexIm[UNWRAP_NUMBER]       = {0.0}; for(int i = 0; i < UNWRAP_NUMBER; i++) curComplexIm[i] = curShiftIm[i];
 
-                if(HEIGHT_WINDOW - counterY > 8){
+                int   exitIndexes[UNWRAP_NUMBER]        = {0}; 
 
-                    float curShift[SIZE_OF_ARRAY] = {0.0};
-
-                    for(int i = 0; i < UNWRAP_NUMBER; i++){
-                        curShift[2 * i]     = (counterX - 1000.0f - time) / 450.0f;
-                        curShift[2 * i + 1] = (counterY + i - 300.0f) / 450.0f;
-                    }
-
-                    char countFlag[UNWRAP_NUMBER] = {0};
-
-                    int curIter = 0;
+                int curIter = 0;
+                
+                if(HEIGHT_WINDOW - counterY > UNWRAP_NUMBER){
                     while(curIter < AMOUNT_ITERATIONS){
-                        countMondelbrot(curComplex + 0 , curShift + 0 , exitIndexes + 0, curIter, countFlag + 0);
-                        countMondelbrot(curComplex + 2 , curShift + 2 , exitIndexes + 1, curIter, countFlag + 1);
-                        countMondelbrot(curComplex + 4 , curShift + 4 , exitIndexes + 2, curIter, countFlag + 2);
-                        countMondelbrot(curComplex + 6 , curShift + 6 , exitIndexes + 3, curIter, countFlag + 3);
-                        countMondelbrot(curComplex + 8 , curShift + 8 , exitIndexes + 4, curIter, countFlag + 4);
-                        countMondelbrot(curComplex + 10, curShift + 10, exitIndexes + 5, curIter, countFlag + 5);
-                        countMondelbrot(curComplex + 12, curShift + 12, exitIndexes + 6, curIter, countFlag + 6);
-                        countMondelbrot(curComplex + 14, curShift + 14, exitIndexes + 7, curIter, countFlag + 7);
+
+                        float curComplexReSquare[UNWRAP_NUMBER] = {0.0}; for(int i = 0; i < UNWRAP_NUMBER; i++) curComplexReSquare[i] = curComplexRe[i] * curComplexRe[i];
+                        float curComplexImSquare[UNWRAP_NUMBER] = {0.0}; for(int i = 0; i < UNWRAP_NUMBER; i++) curComplexImSquare[i] = curComplexIm[i] * curComplexIm[i];
+                        float curComplexImRe[UNWRAP_NUMBER]     = {0.0}; for(int i = 0; i < UNWRAP_NUMBER; i++) curComplexImRe[i]     = curComplexRe[i] * curComplexIm[i];
+
+                        float curComplexSquare[UNWRAP_NUMBER]   = {0.0}; for(int i = 0; i < UNWRAP_NUMBER; i++) curComplexSquare[i]     = curComplexReSquare[i] + curComplexImSquare[i];
+
+                        int curComplexCmp[UNWRAP_NUMBER]        = {0};   for(int i = 0; i < UNWRAP_NUMBER; i++) if(curComplexSquare[i] < MAX_COMPLEX_NUM_SIZE) curComplexCmp[i] = 1;
+
+                        int mask = 0;
+                        for(int i = 0; i < UNWRAP_NUMBER; i++)  mask |= (curComplexCmp[i] << i);
+                        if(!mask) break;
+
+                        for(int i = 0; i < UNWRAP_NUMBER; i++)  exitIndexes[i]  = exitIndexes[i] + curComplexCmp[i];
                         
+                        for(int i = 0; i < UNWRAP_NUMBER; i++)  if((mask >> i) & 1) curComplexRe[i] = (curComplexReSquare[i] - curComplexImSquare[i] + curShiftRe[i]);
+                        for(int i = 0; i < UNWRAP_NUMBER; i++)  if((mask >> i) & 1) curComplexIm[i] = (curComplexImRe[i] + curComplexImRe[i] + curShiftIm[i]);
+
                         curIter++;
                     }
-                
-                    for(size_t i = 0; i < SIZE_OF_ARRAY / 2; i++){
-                        if (counterY + i < HEIGHT_WINDOW) { 
-                            drawMondelbrot(&videoMemBuffer, exitIndexes[i], counterX, counterY + i);
-                        }
+                    
+                    for(int i = 0; i < UNWRAP_NUMBER; i++){
+                        videoMemBuffer[counterX + (-(counterY + i) + HEIGHT_WINDOW - 1) * WIDTH_WINDOW].rgbRed   = exitIndexes[i] % 113;
+                        videoMemBuffer[counterX + (-(counterY + i) + HEIGHT_WINDOW - 1) * WIDTH_WINDOW].rgbGreen = exitIndexes[i] % 128;
+                        videoMemBuffer[counterX + (-(counterY + i) + HEIGHT_WINDOW - 1) * WIDTH_WINDOW].rgbBlue  = exitIndexes[i] % 10;
                     }
-
+                
                 }
-
             }
         }
         txUnlock();
@@ -94,31 +96,4 @@ int main(void){
     }
 
     return 0;
-}
-
-
-inline void countMondelbrot(float* curComplex, float* curShift, int* exitIndexes, int curIter, char* flag){
-    // assert(curComplex);
-    // assert(curShift);
-    // assert(exitIndexes);
-    // assert(flag);
-    
-    float curComplexReSquare = (*curComplex)       * (*curComplex);
-    float curComplexImSquare = (*(curComplex + 1)) * (*(curComplex + 1));
-    float curComplexImRe     = (*(curComplex + 1)) * (*curComplex);
-    
-    int belongsSet           = (curComplexReSquare + curComplexImSquare <= 4.0f); 
-    *exitIndexes += belongsSet;
-
-    *curComplex       = (curComplexReSquare - curComplexImSquare + *curShift);
-    *(curComplex + 1) = 2 * curComplexImRe + *(curShift + 1);
-
-}
-
-inline void drawMondelbrot(RGBQUAD** videoMemBuffer, int curIter, int counterX, int counterY){
-    assert(videoMemBuffer);
-
-    (*videoMemBuffer)[counterX + (-counterY + HEIGHT_WINDOW - 1) * WIDTH_WINDOW].rgbRed   = curIter % 113;
-    (*videoMemBuffer)[counterX + (-counterY + HEIGHT_WINDOW - 1) * WIDTH_WINDOW].rgbGreen = curIter % 128;
-    (*videoMemBuffer)[counterX + (-counterY + HEIGHT_WINDOW - 1) * WIDTH_WINDOW].rgbBlue  = curIter % 10;
 }
